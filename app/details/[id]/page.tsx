92bdb142f97db8e0dd3307edd5ea9db1bd9ca46d
Maksim <fazzzy1234ad@gmail.com>
2025-12-09 00:54:15 +0300

    справлена ошибка типов: изменена инициализация dateTime с '' на undefined

--- app/details/[id]/page.tsx
+++ app/details/[id]/page.tsx
@@ -109,7 +109,7 @@ const DetailsPage: React.FC = () => {
 
   // Извлекаем город и форматируем дату/время
   let city = 'городе';
-  let dateTime: string | { __html: string } = '';
+  let dateTime: string | { __html: string } | undefined = undefined;
   let buyTicketUrl: string | undefined;
 
   if (isFromEvents && event) {
